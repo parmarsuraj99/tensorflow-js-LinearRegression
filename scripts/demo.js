817e6132dabf88c8000ddfc8ea0d1fb23580323b
Suraj Parmar <parmarsuraj99@gmail.com>
2019-03-13 21:18:48 +0530

    improved model

--- scripts/demo.js
+++ scripts/demo.js
@@ -2,23 +2,25 @@
 // because of the script tag above.
 
 // Define a model for linear regression.
+
 const model = tf.sequential();
-const model1 = tf.models.modelFromJSON('models/model.json');
+//const model1 = tf.loadModel('https://github.com/parmarsuraj99/tensorflow-js-mnist/blob/master/scripts/models/model.json');
 
+ model.add(tf.layers.dense({inputShape: [1], units: 1, useBias: true}));
 
-model.add(tf.layers.dense({units: 1, inputShape: [1]}));
 
 // Prepare the model for training: Specify the loss and the optimizer.
-model.compile({loss: 'meanSquaredError', optimizer: 'sgd'});
+model.compile({loss: 'meanSquaredError', optimizer: 'sgd', metrics: ['mse']});
 
 // Generate some synthetic data for training.
-const xs = tf.tensor2d([1, 2, 3, 4], [4, 1]);
+const xs = tf.tensor2d([1, 2, 3, 4, 5], [5, 1]);
+document.getElementById("X").innerHTML="X: "+xs;
 
-const ys = tf.tensor2d([1, 3, 5, 7], [4, 1]);
-
+const ys = tf.tensor2d([2, 4, 6, 8, 10], [5, 1]);
+document.getElementById("Y").innerHTML="Y: "+ys;
 // Train the model using the data.
-model.fit(xs, ys);
+model.fit(xs, ys, {epochs: 50});
 // Use the model to do inference on a data point the model hasn't seen before:
 // Open the browser devtools to see the output
 
-document.getElementById("isLoaded").innerText=model.predict(tf.tensor2d([5], [1, 1]));
+document.getElementById("isLoaded").innerText="Prediction(5): "+model.predict(tf.tensor([6]));

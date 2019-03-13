fc5231cdf1c3972584d7c953fcfdb6e52d50b46a
Suraj Parmar <parmarsuraj99@gmail.com>
2019-03-13 20:06:41 +0530

    Initial Commit

--- scripts/demo.js
+++ scripts/demo.js
@@ -3,7 +3,7 @@
 
 // Define a model for linear regression.
 const model = tf.sequential();
-const model1 = await tf.models.modelFromJSON('models/model.json');
+const model1 = tf.models.modelFromJSON('models/model.json');
 
 
 model.add(tf.layers.dense({units: 1, inputShape: [1]}));
@@ -13,6 +13,7 @@ model.compile({loss: 'meanSquaredError', optimizer: 'sgd'});
 
 // Generate some synthetic data for training.
 const xs = tf.tensor2d([1, 2, 3, 4], [4, 1]);
+
 const ys = tf.tensor2d([1, 3, 5, 7], [4, 1]);
 
 // Train the model using the data.
